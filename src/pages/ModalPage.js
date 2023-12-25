5cf10d6da5c8a8a266e9b4a6fc4632364825592d
Sydnee <svwoodhouse@gmail.com>
2023-12-25 16:38:01 -0500

    Added Modal component

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,58 @@
+import Button from "../components/Button";
 import Modal from "../components/Modal";
+import { useState } from "react";
 
 function ModalPage () {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleClick = () => {
+        setShowModal(true)
+    }
+
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
+    const actionBar = <div><Button primary onClick={handleClose}>I Accept</Button></div>
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>Here is an important agreement for you to accept</p>
+    </Modal>
+
     return (
-        <div>
-            <Modal></Modal>
+        <div className="relative">
+            <Button primary onClick={handleClick}>Open Modal</Button>
+            {showModal && modal}
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam beatae expedita explicabo commodi reprehenderit, illo minus consequuntur dolorem provident in omnis deserunt esse aut ipsam quasi sapiente molestias neque!
+            </p>
         </div>
     )
 }
